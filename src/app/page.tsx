e05dc50046b80b8ec4a2623e8ee76bd4421941cc
Erik Hawerman <erik.hawerman@hiq.se>
2024-06-03 13:18:06 +0200

    added confetti removed progressbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
 "use client";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import ProgressBar from "./components/ProgressBar";
+import Confetti from "react-confetti";
 
 const countVariants = {
   updated: { scale: 1.2, transition: { duration: 0.3 } },
   initial: { scale: 1 },
 };
+const milestoneVariants = {
+  visible: { opacity: 1, y: 100, transition: { duration: 0.1 }, scale: 2.0 },
+  hidden: { opacity: 0, y: -50 },
+};
+const milestones = Array.from({ length: 100 }, (_, i) => (i + 1) * 1000);
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 export default function HomePage() {
   const [count, setCount] = useState(0);
-  const goal = 100;
   const [animation, setAnimation] = useState("initial");
-
-  console.log(
-    "process.env.COUNT_API_URL",
-    process.env.NEXT_PUBLIC_COUNT_API_URL,
-  );
+  const [milestone, setMilestone] = useState<number | null>(null);
+  const [lastFetchedCount, setLastFetchedCount] = useState(0);
 
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_COUNT_API_URL) return;
     const fetchCount = () => {
-      fetch(process.env.NEXT_PUBLIC_COUNT_API_URL!)
-        .then((res) => res.json())
+      fetch(process.env.NEXT_PUBLIC_COUNT_API_URL!, { mode: "cors" })
+        .then((res) => {
+          return res.json();
+        })
         .then((data: number) => {
           if (data > count) {
             setAnimation("updated");
@@ -45,8 +51,34 @@ export default function HomePage() {
     }
   }, [animation]);
 
+  useEffect(() => {
+    setLastFetchedCount(count);
+    milestones.forEach((m) => {
+      if (count >= m && lastFetchedCount < m) {
+        setMilestone(m);
+      }
+    });
+  }, [count, lastFetchedCount]);
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
+      {milestone && (
+        <>
+          <Confetti width={width} height={height} />
+          <motion.span
+            className="absolute top-10 rounded p-4 text-8xl font-bold text-yellow-400"
+            variants={milestoneVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            onAnimationComplete={() =>
+              setTimeout(() => setMilestone(null), 10000)
+            }
+          >
+            {milestone}
+          </motion.span>
+        </>
+      )}
       <motion.span
         className="inline-block rounded-lg p-4 text-[12rem] font-semibold text-blue-500"
         variants={countVariants}
@@ -54,7 +86,6 @@ export default function HomePage() {
       >
         {count}
       </motion.span>
-      <ProgressBar progress={(count / goal) * 100} />
     </div>
   );
 }
